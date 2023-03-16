da8b782b99754fcd777857a4efc8ecd23d5e4bb7
Jan Vítů <vitu.jan1@gmail.com>
2023-03-16 17:56:55 +0100

    Solid-JS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  content: ["./**/*.{html,md,svg}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          100: "#1A1C33",
-          dark: "#252525",
-        },
-        crimson: "#D80032",
-        sunset: {
-          50: "#ffebee",
-          100: "#ffccd2",
-          200: "#f99898",
-          300: "#f36e6f",
-          // primary
-          400: "#fe4949",
-          500: "#ff322a",
-          600: "#f5262b",
-          700: "#e31725",
-          800: "#d6061e",
-          900: "#c60010",
-        },
-      },
-      zIndex: {
-        "-10": "-10",
-        "-1": "-1",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./hugo/**/*.{html,md,svg,tsx,ts}", "./app/**/*.{tsx,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					100: "#1A1C33",
+					dark: "#252525",
+				},
+				crimson: "#D80032",
+				sunset: {
+					50: "#ffebee",
+					100: "#ffccd2",
+					200: "#f99898",
+					300: "#f36e6f",
+					// primary
+					400: "#fe4949",
+					500: "#ff322a",
+					600: "#f5262b",
+					700: "#e31725",
+					800: "#d6061e",
+					900: "#c60010",
+				},
+			},
+			zIndex: {
+				"-10": "-10",
+				"-1": "-1",
+			},
+		},
+	},
+	plugins: [],
 };
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { readdir, rm, writeFile, copyFile } from "fs/promises";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	root: "app",
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: "esnext",
+	},
+	plugins: [solidPlugin(), syncToHugo()],
+});
+
+function syncToHugo() {
+	return {
+		closeBundle: async () => {
+			const assetsJsDir = "./app/dist/assets";
+			const assets = await readdir(assetsJsDir);
+			const js = assets.filter((name) => name.match(/(index*.).*\w+/))[0];
+			await Promise.all([
+				copyFile(
+					`./app/dist/assets/${js}`,
+					"./hugo/assets/js/CookieBanner/index.js",
+				),
+			]);
+
+			console.log(`wrote ${js} to hugo static`);
+		},
+	};
+}
