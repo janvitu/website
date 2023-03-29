c3118adfdd44b4d02499955e3a6289ba49987963
Jan Vítů <vitu.jan1@gmail.com>
2023-03-29 12:10:32 +0200

    Cookie banner changes

--- app/src/components/Dialog/SlideRouter.tsx
+++ app/src/components/Dialog/SlideRouter.tsx
@@ -1,29 +1,54 @@
-export const SlideRouter = ({ setActiveSlide }: any) => {
+import { LangData } from "../../../app";
+import { Accessor, Setter } from "solid-js";
+
+type SlideRouter = {
+	setActiveSlide: Setter<string>;
+	activeSlide: Accessor<string>;
+	content: LangData["cookieDialog"];
+};
+
+export const SlideRouter = ({
+	setActiveSlide,
+	activeSlide,
+	content,
+}: SlideRouter) => {
 	return (
 		<div class="grid grid-cols-3 gap-4 px-3">
 			<button
-				class="p-2"
+				class={`p-2 decoration-amber-400 hover:text-neutral-900 ${
+					activeSlide() === "consent"
+						? "border-b-2 border-neutral-800 font-bold"
+						: "hover:underline"
+				}`}
 				onClick={() => {
 					setActiveSlide("consent");
 				}}
 			>
-				Consent
+				{content.consent.key}
 			</button>
 			<button
-				class="p-2"
+				class={`p-2 decoration-amber-400 hover:text-neutral-900 ${
+					activeSlide() === "detail"
+						? "border-b-2 border-neutral-800 font-bold"
+						: "hover:underline"
+				}`}
 				onClick={() => {
 					setActiveSlide("detail");
 				}}
 			>
-				Details
+				{content.details.key}
 			</button>
 			<button
-				class="p-2"
+				class={`p-2 decoration-amber-400 hover:text-neutral-900 ${
+					activeSlide() === "about"
+						? "border-b-2 border-neutral-800 font-bold"
+						: "hover:underline"
+				}`}
 				onClick={() => {
 					setActiveSlide("about");
 				}}
 			>
-				About
+				{content.about.key}
 			</button>
 		</div>
 	);
