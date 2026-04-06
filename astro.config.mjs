eec866921bf3079c5c427d315296cf5aa1097ec8
Jan Vítů <vitu.jan1@gmail.com>
2026-04-06 14:34:28 +0200

    chore(ts): switch tsconfig to astro strict preset and update astro output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://janvitu.com",
-  output: "hybrid",
+  output: "static",
   adapter: netlify(),
   trailingSlash: "always",
   build: {
